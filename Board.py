ce5c04781343b7af9720303c34664fe255e08d77
carterghill <carterghill@gmail.com>
2018-03-04 16:10:29 -0600

    Added Dragon, Queen, and Wight classes Board now initializes with those classes in their proper position in the grid

--- Board.py
+++ Board.py
@@ -1,3 +1,8 @@
+from Queen import Queen
+from Dragon import Dragon
+from Wight import Wight
+
+
 class Board:
 
     def __init__(self):
@@ -5,6 +10,23 @@ class Board:
         # The board grid will be a 5x5 2d list of cells
         # Each cell will be a list of characters occupying it
         self.grid = [[[] for x in range(5)] for y in range(5)]
+        self.add(Queen(), 0, 2)
+        self.add(Dragon(), 1, 1)
+        self.add(Dragon(), 1, 2)
+        self.add(Dragon(), 1, 3)
+        self.add(Wight(), 4, 1)
+        self.add(Wight(), 4, 2)
+        self.add(Wight(), 4, 3)
+        self.add(Wight(), 4, 0)
+        self.add(Wight(), 4, 4)
+
+    def add(self, ent, x, y):
+
+        assert x >= 0 and x <= 4, "x must be in range [0,4], x = " + str(x)
+        assert y >= 0 and y <= 4, "y must be in range [0,4], y = " + str(y)
+
+        self.grid[x][y].append(ent)
+        ent.set_coordinates(x, y)
 
     def to_string(self):
 
@@ -16,6 +38,17 @@ class Board:
                 # If no one is on the cell, its length will be 0
                 if len(cell) == 0:
                     words = words + " - "
+
+                # If no one is on the cell, its length will be 0
+                if len(cell) == 1:
+                    if type(cell[0]) is Queen:
+                        words = words + " Q "
+                    elif type(cell[0]) is Dragon:
+                        words = words + " D "
+                    elif type(cell[0]) is Wight:
+                        words = words + " W "
+                    else:
+                        words = words + str(type(cell[0]))
 
             words = words + "\n"
 
---
+++ Queen.py
@@ -0,0 +1,13 @@
+class Queen:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def set_coordinates(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_coordinates(self):
+        return self.x, self.y
+
